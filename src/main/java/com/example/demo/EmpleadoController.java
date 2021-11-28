8a51aee838b5d6ec5bb971398cb42a9035a4f1a9
Alexis3004 <nelsonalexiscacerzcarreo37@gmail.com>
2021-11-28 17:28:58 -0500

    controller

--- src/main/java/com/example/demo/EmpleadoController.java
+++ src/main/java/com/example/demo/EmpleadoController.java
@@ -2,8 +2,6 @@ package com.example.demo;
 
 import java.util.ArrayList;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,7 +10,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.HttpClientErrorException;
 
 @RestController
 @RequestMapping("/")
